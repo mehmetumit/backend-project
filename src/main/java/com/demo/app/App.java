280b582bfe7304abf70e7137a3dfc8c5021f84a8
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2022-08-18 15:50:39 +0300

    REASTEasy added to maven

--- src/main/java/com/demo/app/App.java
+++ src/main/java/com/demo/app/App.java
@@ -1,36 +1,8 @@
 package com.demo.app;
 
-import java.sql.SQLException;
-import java.util.List;
-
-import com.demo.app.repository.DatabaseEngine;
-import com.demo.app.repository.daoImpl.CustomerDAOImpl;
-import com.demo.app.services.abstracts.CustomerService;
-import com.demo.app.services.abstracts.OrderService;
-import com.demo.app.services.implementations.CustomerServiceImpl;
-import com.demo.app.services.implementations.OrderServiceImpl;
-import com.demo.app.models.dtos.CustomerDTO;
-import com.demo.app.models.entities.*;
-
-import org.hibernate.Session;
-import org.hibernate.sql.Delete;
-
 class App {
 
 	public static void main(String[] args) {
-		DatabaseEngine dbEngine = DatabaseEngine.getEngine();
-		CustomerServiceImpl customerService = new CustomerServiceImpl();
-		OrderServiceImpl orderService = new OrderServiceImpl();
-		List<CustomerDTO> customers = customerService.getAll();
-		// customers.forEach(c -> System.out.println(c));
 
-		System.out.println(orderService.getById(1).getOrderDetails());
-
-		// dbEngine.closeDB();
-
-		// Fix mvn exec plugin thread issue
-		// Also we can add "<cleanupDaemonThreads>false</cleanupDaemonThreads>" option
-		// to fix the issue instead of System.exit(0)
-		// System.exit(0);
 	}
 }
